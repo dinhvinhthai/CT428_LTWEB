5ee45e459d8a32a1e04dd09749287229e08f111f
Đinh Vĩnh Thái <dinhvinhthai.dev@gmail.com>
2022-04-07 00:05:35 +0700

    Update by Thai

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-function showBio() {
-    document.getElementById("bio").style.display = "block";
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("books").style.display = "none";
-    document.getElementById("ct428").style.display = "none";
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction();myFunction2()};
+
+// Get the header
+var header = document.getElementById("navbar");
+var header2= document.getElementById("navbar-icon");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
   }
-  function showIntro() {
-    document.getElementById("bio").style.display = "none";
-    document.getElementById("intro").style.display = "block";
-    document.getElementById("books").style.display = "none";
-    document.getElementById("ct428").style.display = "none";
+}
+
+function myFunction2() {
+  if (window.pageYOffset > sticky) {
+    header2.classList.add("sticky2");
+  } else {
+    header2.classList.remove("sticky2");
   }
-  function showBooks() {
-    document.getElementById("bio").style.display = "none";
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("books").style.display = "block";
-    document.getElementById("ct428").style.display = "none";
-  }
-  function showCT428() {
-    document.getElementById("bio").style.display = "none";
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("books").style.display = "none";
-    document.getElementById("ct428").style.display = "block";
-  }
+}
